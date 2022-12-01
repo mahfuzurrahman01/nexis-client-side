a46f53fceb171f551f0b95fe945962eb659c0bad
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-12-01 20:23:15 +0600

    daisy ui setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,21 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  themes: [
+    {
+      NexisTheme: {
+
+        "primary": "#1678CB",
+
+        "secondary": "#3B8BEA",
+
+        "accent": "#242424",
+
+        "neutral": "#68D6F5",
+
+        "base-100": "#FFFFFF",
+      },
+    },
+  ],
+  plugins: [require("daisyui")],
 }
